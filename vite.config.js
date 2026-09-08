c182463f6e622c7dee70bcdaebb3d66da3839f68
Jade2308 <vinhsanh2308@gmail.com>
2026-09-08 23:44:48 +0700

    chore: rename PWA app name to Victory Plan

--- vite.config.js
+++ vite.config.js
@@ -19,8 +19,8 @@ export default defineConfig({
         'victory-plan-mark.svg'
       ],
       manifest: {
-        name: 'Chiến Thắng - Kế Hoạch Đọc Kinh Thánh',
-        short_name: 'Chiến Thắng',
+        name: 'Victory Plan - Kế Hoạch Đọc Kinh Thánh',
+        short_name: 'Victory Plan',
         description: 'Victory Bible Reading Plan - 365 ngày đồng hành cùng Lời Chúa',
         theme_color: '#112019',
         background_color: '#0b1410',
